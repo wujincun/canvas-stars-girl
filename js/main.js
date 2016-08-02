196e603af2781d9b82c9e7b80e931b03544ef706
wujincun <1769453779@qq.com>
2016-08-02 22:28:54 +0800

    star

--- js/main.js
+++ js/main.js
@@ -1,34 +1,49 @@
 /**
  * Created by wujincun on 2016/8/2.
  */
-var can,ctx,w,h;
+var can, ctx, w, h;
 var girlPic = new Image();
 var starPic = new Image();
-function init(){
+
+var num = 60;
+var starts = [];
+
+var lastTime, deltaTime;
+function init() {
     can = document.getElementById('canvas');
     ctx = can.getContext('2d');
     w = can.width;
     h = can.height;
 
-    girlPic.src="src/girl.jpg";
-    starPic.src="src/star.png";
+    girlPic.src = "src/girl.jpg";
+    starPic.src = "src/star.png";
+
+    for (var i = 0; i < num; i++) {
+        var obj = new StarObj();
+        starts.push(obj);
+        starts[i].init()
+    }
+    lastTime = Date.now();
 
     gameLoop()
 }
 document.body.onload = init;
-function gameLoop(){
+function gameLoop() {
     window.requestAnimFrame(gameLoop);
+    var now = Date.now();
+    deltaTime = now - lastTime;
+    lastTime = now;
     drawBackground();
     drawGirl();
-    drawStar();
+    drawStars();
 }
 
-function drawBackground(){
+function drawBackground() {
     ctx.fillStyle = '#393550';
-    ctx.fillRect(0,0,w,h)
+    ctx.fillRect(0, 0, w, h)
 }
 function drawGirl() {
     //drawImage(img,x,y，width,height)
     //x轴标正方向向右，y轴坐标正方向向下，（0,0）在canvas左上角
-    ctx.drawImage(girlPic,100,150,600,300)
+    ctx.drawImage(girlPic, 100, 150, 600, 300)
 }
--- js/stars.js
+++ js/stars.js
@@ -1,7 +1,35 @@
 /**
  * Created by wujincun on 2016/8/2.
  */
-
-function drawStar(){
-    ctx.drawImage(starPic,300,400)
+function StarObj() {
+    this.x;
+    this.y;
+    this.picNo;
+    this.timer;
 }
+StarObj.prototype.init = function () {
+    this.x = Math.random() * 600 + 100;
+    this.y = Math.random() * 300 + 150;
+    this.picNo = 0;
+    this.timer = 0;
+};
+StarObj.prototype.update = function () {
+    this.timer += deltaTime;
+    if(this.timer > 50){
+        this.picNo += 1;
+        this.picNo %= 7;
+        this.timer = 0;
+    }
+}
+StarObj.prototype.draw = function () {
+    //drawImage(img,sx,sy,swidth,sheight,x,y,width,height)
+    ctx.drawImage(starPic, this.picNo * 7, 0, 7, 7, this.x, this.y, 7, 7)
+};
+function drawStars() {
+    for (var i = 0; i < num; i++) {
+        starts[i].update();
+        starts[i].draw()
+    }
+}
+
+
